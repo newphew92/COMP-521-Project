0f86cecff40fb534b34e74085dcc9902fc5479c6
Nicholas Nathan Colotouros <agentnnc@gmail.com>
2015-03-28 18:17:11 -0400

    laser prefab

---
+++ Workspace/Assets/LaserScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserScript : MonoBehaviour 
+{
+	public float damage = 20f;
+
+	void OnCollisionEnter(Collision collision)	
+	{
+		if (collision.transform.tag == "Robot")
+		{
+			collision.transform.GetComponent<UnitProperties>().HP -= damage;
+		}
+		Destroy (gameObject);
+	}
+}
---
+++ Workspace/Assets/Scripts/UnitProperties.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitProperties : MonoBehaviour 
+{
+	public float HP = 100f;
+	public float FiringRate = 3f;
+}
