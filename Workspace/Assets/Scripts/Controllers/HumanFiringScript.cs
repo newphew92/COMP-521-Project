56944b66037aa127a846cace44868efbf65b9d7e
Nicholas Nathan Colotouros <agentnnc@gmail.com>
2015-03-28 20:59:43 -0400

    firing and first person progress

---
+++ Workspace/Assets/Scripts/Controllers/HumanFiringScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanFiringScript : MonoBehaviour 
+{
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			UnitProperties p = gameObject.GetComponent<UnitProperties>();
+			p.shoot(p.BulletSpawnPoint.forward);
+		}
+	}
+}
--- Workspace/Assets/Scripts/UnitProperties.cs
+++ Workspace/Assets/Scripts/UnitProperties.cs
@@ -4,5 +4,30 @@ using System.Collections;
 public class UnitProperties : MonoBehaviour 
 {
 	public float HP = 100f;
-	public float FiringRate = 3f;
+	public float FiringRate = 3f; // in shots per second
+	private float BulletStrength = 10000f;
+
+	private float ShotCooldown = 0f;
+
+	public Transform Bullet;
+	public Transform BulletSpawnPoint;
+
+	void Update()
+	{
+		if (ShotCooldown > 0)
+			ShotCooldown -= Time.deltaTime;
+	}
+
+	public void shoot(Vector3 direction)
+	{
+		if (ShotCooldown <= 0) 
+		{
+//			Quaternion rotation = Quaternion.FromToRotation(Bullet.forward, BulletSpawnPoint.position.forward);
+			Transform shotFired = Instantiate (Bullet, BulletSpawnPoint.position, rotation) as Transform;
+
+			shotFired.GetComponent<Rigidbody> ().AddForce (direction * BulletStrength);
+
+			ShotCooldown = FiringRate;
+		}
+	}
 }
