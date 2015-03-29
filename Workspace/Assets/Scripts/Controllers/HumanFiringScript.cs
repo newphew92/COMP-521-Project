9600bec28c7c48aba7ad3da44426b2d13e62dec0
Nicholas Nathan Colotouros <agentnnc@gmail.com>
2015-03-29 09:22:14 -0400

    fixed firing

--- Workspace/Assets/Scripts/Controllers/HumanFiringScript.cs
+++ Workspace/Assets/Scripts/Controllers/HumanFiringScript.cs
@@ -1,47 +1,47 @@
-﻿//using UnityEngine;
-//using UnityEngine.UI;
-//using System.Collections;
-//
-//public class HumanFiringScript : MonoBehaviour 
-//{
-//	public Transform textTransform;
-//	private Text t;
-//	private bool alive = true;
-//	private Transform Robots;
-//	private UnitProperties Prop;
-//
-//	void Start()
-//	{
-//		t = textTransform.GetComponent<Text> ();
-//		Robots = GameObject.Find ("Robots").transform;
-//		Prop = gameObject.GetComponent<UnitProperties>();
-//	}
-//
-//	// Update is called once per frame
-//	void Update () 
-//	{
-//		if(Input.GetMouseButton(0) && alive)
-//		{
-//			Prop.shoot(Prop.BulletSpawnPoint.forward);
-//		}
-//
-//		if( gameObject.GetComponent<UnitProperties>().HP <= 0 )
-//		{
-//			t.color = Color.red;
-//			t.text = "YOU LOSE";
-//			alive = false;
-//		}
-//		else if( alive && Robots.childCount == 0 )
-//		{
-//			t.text = "YOU WIN";
-//			t.color = Color.green;
-//			Invoke("NextLevel", 3);
-//		}
-//	}
-//
-//	private void NextLevel()
-//	{
-//		int level = Application.loadedLevel;
-//		Application.LoadLevel (level + 1);
-//	}
-//}
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HumanFiringScript : MonoBehaviour 
+{
+	public Transform textTransform;
+	private Text t;
+	private bool alive = true;
+	private Transform Robots;
+	private UnitProperties Prop;
+
+	void Start()
+	{
+		t = textTransform.GetComponent<Text> ();
+		Robots = GameObject.Find ("Robots").transform;
+		Prop = gameObject.GetComponent<UnitProperties>();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetMouseButton(0) && alive)
+		{
+			Prop.shoot(Prop.BulletSpawnPoint.forward);
+		}
+
+		if( gameObject.GetComponent<UnitProperties>().HP <= 0 )
+		{
+			t.color = Color.red;
+			t.text = "YOU LOSE";
+			alive = false;
+		}
+		else if( alive && Robots.childCount == 0 )
+		{
+			t.text = "YOU WIN";
+			t.color = Color.green;
+			Invoke("NextLevel", 3);
+		}
+	}
+
+	private void NextLevel()
+	{
+		int level = Application.loadedLevel;
+		Application.LoadLevel (level + 1);
+	}
+}
---
+++ Workspace/Assets/Scripts/UnitProperties.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitProperties : MonoBehaviour 
+{
+	public float HP = 100f;
+	public float FiringRate = 0.5f; // in shots per second
+	
+	public Transform Bullet;
+	public Transform BulletSpawnPoint;
+	
+	private float BulletStrength = 10000f;
+	private float ShotCooldown = 0f;
+	
+	void Update()
+	{
+		FiringRate = 0.5f;
+		if (ShotCooldown > 0)
+			ShotCooldown -= Time.deltaTime;
+	}
+	
+	public void shoot(Vector3 direction)
+	{
+		if (ShotCooldown <= 0) 
+		{
+			Transform shotFired = Instantiate (Bullet, BulletSpawnPoint.position, this.transform.rotation) as Transform;
+			shotFired.Rotate(new Vector3(90,0,0));
+			shotFired.GetComponent<Rigidbody> ().AddForce (direction * BulletStrength);
+			
+			ShotCooldown = FiringRate;
+			AudioSource a = gameObject.GetComponent<AudioSource>();
+			a.PlayOneShot(a.clip);
+			Debug.Log(Random.Range(0,100));
+		}
+	}
+}
