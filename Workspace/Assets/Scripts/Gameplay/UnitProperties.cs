6e42b82162920716f650897f6dcd6cc945fc2016
Nicholas Nathan Colotouros <agentnnc@gmail.com>
2015-04-04 14:09:55 -0400

    implemented firing at closest unit

--- Workspace/Assets/Scripts/Gameplay/UnitProperties.cs
+++ Workspace/Assets/Scripts/Gameplay/UnitProperties.cs
@@ -10,6 +10,7 @@ public class UnitProperties : MonoBehaviour
 
 	public float HP = 100f;
 	public float FiringRate = 0.5f; // in shots per second
+	public float FiringRadius = 1f;
 	
 	public Transform Bullet;
 	public Transform BulletSpawnPoint;
@@ -24,11 +25,67 @@ public class UnitProperties : MonoBehaviour
 
 		if (ShotCooldown > 0)
 			ShotCooldown -=Time.deltaTime;
-		
-		UpdatePosition ();
+
+		if(ShotCooldown <= 0)
+			Fire ();
+
+		//UpdatePosition ();
 	}
-	
-	public void shoot(Transform target)
+
+	private void Fire()
+	{
+		Transform[] enemies = GetEnemies();
+		Transform closestEnemy = GetClosestUnit (enemies);
+
+		if (closestEnemy != null)
+			ShootAt (closestEnemy);
+	}
+
+	private Transform[] GetEnemies()
+	{
+		PlayerManager pm = GameObject.Find ("Level").GetComponent<PlayerManager> ();
+
+		if (PlayerSide == Team.Blue)
+			return pm.RedPlayer;
+
+		return pm.BluePlayer;
+	}
+
+	private Transform GetClosestUnit(Transform[] enemies)
+	{
+		float closestDist = 99999f;
+		Transform closestUnit = null;
+
+		Vector3 pos = transform.position;
+		for( int i = 0; i < enemies.Length; i++)
+		{
+			Team enemyTeam = enemies [i].GetComponent<UnitProperties> ().PlayerSide;
+			if( enemies[i] != null )
+			{
+				Vector3 enemyPos = enemies[i].position;
+
+				RaycastHit hit;
+				LayerMask mask = ~(1 << LayerMask.NameToLayer (enemyTeam.ToString()));
+				if(Physics.Raycast(enemyPos + Vector3.up, pos - enemyPos, out hit, FiringRadius, mask))
+				{
+					// raycast from enemy to unit successfull --> in firing range
+					if( hit.transform.position == transform.position )
+					{
+						float dist = Vector3.Distance( pos, enemyPos );
+						if( dist < closestDist )
+						{
+							closestDist = dist;
+							closestUnit = enemies[i];
+						}
+					}
+				}
+			}
+		}
+
+		return closestUnit;
+	}
+
+	private void ShootAt(Transform target)
 	{
 		if (ShotCooldown <= 0) 
 		{
