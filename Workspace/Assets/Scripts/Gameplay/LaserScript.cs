588e29160837d72732b696b830dc0177801cde7e
Nicholas Nathan Colotouros <agentnnc@gmail.com>
2015-04-03 23:17:29 -0400

    unit properties and shooting revamped

---
+++ Workspace/Assets/Scripts/Gameplay/LaserScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserScript : MonoBehaviour 
+{
+	public AudioClip[] laserFire;
+	public Transform Destination;
+	public float damage = 20f;
+	private float timeout = 5f;
+
+	void Start()
+	{
+		AudioSource source = GetComponent<AudioSource> ();
+		source.PlayOneShot (laserFire [Random.Range (0, laserFire.Length)]);
+	}
+	
+	void Update()
+	{
+		if(timeout <= 0) Destroy (gameObject);
+		timeout -= Time.deltaTime;
+	}
+	
+	void OnCollisionEnter(Collision collision)	
+	{
+		UnitProperties p = Destination.GetComponent<UnitProperties> ();
+		if (p != null)
+			p.HP -= damage;
+
+		GetComponent<CapsuleCollider> ().enabled = false;
+		GetComponent<MeshRenderer> ().enabled = false;
+	}
+}
---
+++ Workspace/Assets/Scripts/Gameplay/UnitProperties.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitProperties : MonoBehaviour 
+{
+	public float HP = 100f;
+	public float FiringRate = 0.5f; // in shots per second
+	
+	public Transform Bullet;
+	public Transform BulletSpawnPoint;
+	
+	private float BulletStrength = 10f;
+	private float ShotCooldown = 0f;
+
+	void Update()
+	{
+		if (ShotCooldown > 0)
+			ShotCooldown -=Time.deltaTime;
+
+		if (HP<=0)
+			Destroy(gameObject);
+	}
+	
+	public void shoot(Transform target)
+	{
+		if (ShotCooldown <= 0) 
+		{
+			Vector3 direction = target.position - transform.position;
+			Transform shotFired = Instantiate (Bullet, BulletSpawnPoint.position, this.transform.rotation) as Transform;
+			shotFired.Rotate(new Vector3(90,0,0));
+			shotFired.GetComponent<Rigidbody> ().AddForce (direction * BulletStrength);
+			
+			ShotCooldown = FiringRate;
+		}
+	}	
+}
