ab7ac3566031c5caa17bcc2056963f80def2566c
Nicholas Nathan Colotouros <agentnnc@gmail.com>
2015-04-04 18:19:02 -0400

    start of terrain analysis

---
+++ Workspace/Assets/Scripts/Terrain/HeightAnalyzer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// TODO
+public class HeightAnalyzer : AbstractTerrainAnalyzer 
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
