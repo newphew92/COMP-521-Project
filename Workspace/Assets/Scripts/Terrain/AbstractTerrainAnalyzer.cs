40e0b72d9a42a29d5c2047790d5133fe455397a5
Nicholas Nathan Colotouros <agentnnc@gmail.com>
2015-04-07 21:42:29 -0400

    implemented naive height based terrain analysis

--- Workspace/Assets/Scripts/Terrain/AbstractTerrainAnalyzer.cs
+++ Workspace/Assets/Scripts/Terrain/AbstractTerrainAnalyzer.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public abstract class AbstractTerrainAnalyzer : MonoBehaviour 
 {
 	public Transform[,] level; // insert board before doing anything
+	public float maxTerrainHeat; // must be > 0
 
 	// goes through the level and analyzes it (adds in the basic heat values) while ignoring units
 	public abstract void AnalyzeTerrain();
--- Workspace/Assets/Scripts/Terrain/HeightAnalyzer.cs
+++ Workspace/Assets/Scripts/Terrain/HeightAnalyzer.cs
@@ -4,6 +4,35 @@ using System.Collections;
 // TODO
 public class HeightAnalyzer : AbstractTerrainAnalyzer 
 {
-	public override void AnalyzeTerrain(){}
+	public override void AnalyzeTerrain()
+	{
+		float maxHeight = GetHeightExtreme ();
+		float heatIncrease = maxTerrainHeat / maxHeight;
+
+		for( int i = 0; i < level.GetLength(0); i++)
+		{
+			for( int j = 0; j < level.GetLength(1); j++)
+			{
+				Transform tile = level[i,j];
+				tile.GetComponent<TileProperties>().BaseHeat = heatIncrease * tile.pos.y;
+			}
+		}
+	}
+
+	private float GetHeightExtreme()
+	{
+		float max = 0;
+		for( int i = 0; i < level.GetLength(0); i++)
+		{
+			for( int j = 0; j < level.GetLength(1); j++)
+			{
+				int currentHeight = Mathf.Abs(level[i,j].transform.position.y);
+				if(currentHeight > max)
+					max = currentHeight;
+			}
+		}
+	}
+
+	// TODO
 	public override Transform[][] GetChokePoints(){return null;}
 }
