e6b4ec9fae763295a26b3ec9cfa2e4572367fb7a
Nicholas Nathan Colotouros <agentnnc@gmail.com>
2015-04-10 01:17:04 -0400

    more progress while waiting on 361

--- Workspace/Assets/Scripts/Terrain/Analyzers/ViewDistanceAnalyzer.cs
+++ Workspace/Assets/Scripts/Terrain/Analyzers/ViewDistanceAnalyzer.cs
@@ -6,15 +6,20 @@ public class ViewDistanceAnalyzer : AbstractTerrainAnalyzer
 {
 	public int UnitViewRadius;
 	private int[,] ViewDistances; // num tiles seen from each given point
+	private Transform testUnit; // used for testing raycasts
 
 	public override void AnalyzeTerrain()
 	{
+		Transform prefab = Resources.Load ("TerrainGen/Floor") as Transform;
+		testUnit = Instantiate (testUnit, Vector3.zero, Quaternion.identity) as Transform;
+
 		int length = level.GetLength (0);
 		int width = level.GetLength (1);
 		ViewDistances = new int[length, width];
 
 		FillInViewDistances ();
 		// TODO: fill in heat
+		Destroy (testUnit.gameObject);
 	}
 
 	void FillInViewDistances ()
@@ -34,6 +39,8 @@ public class ViewDistanceAnalyzer : AbstractTerrainAnalyzer
 		Transform tile = level [i, j].transform;
 		int canSee = 1; // because the tile itself can be seen
 
+		testUnit.transform.position = tile.position + Vector3.up;
+
 		for( int x = 0; i < level.GetLength(0); i++)
 		{
 			for( int y = 0; j < level.GetLength(1); j++)
@@ -45,7 +52,14 @@ public class ViewDistanceAnalyzer : AbstractTerrainAnalyzer
 
 				if(currentlyObserving.position.y - tile.position.y < 1)
 				{
-					// TODO raycast
+					RaycastHit hit;
+					Vector3 origin = currentlyObserving.position + Vector3.up * 0.5f;
+					Vector3 direction = tile.position - origin;
+					if( Physics.Raycast( origin, direction, out hit, UnitViewRadius))
+					{
+						if(hit.transform.name == "Floor");
+							canSee++;
+					}
 				}
 			}
 		}
