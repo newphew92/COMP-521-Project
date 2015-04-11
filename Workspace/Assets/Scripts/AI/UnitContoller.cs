1fb1cc7af937457f64affb5c55298492a522bd2b
newphew92 <terrenceko@outlook.com>
2015-04-11 02:23:53 -0400

    Added squad management, threat assessment and tags.

--- Workspace/Assets/Scripts/AI/UnitContoller.cs
+++ Workspace/Assets/Scripts/AI/UnitContoller.cs
@@ -15,8 +15,14 @@ public class UnitContoller : MonoBehaviour {
 	public float Sign = -1;
 	public CastDown tileCheck;
 	public bool Scanning;
+
+	public SquadManager Manager;
+
+	public int Enemies;
+	public int Allies;
 	// Use this for initialization
 	void Start () {
+		Manager = gameObject.GetComponentInParent<SquadManager> ();
 		Scanning = true;
 		tileCheck = gameObject.GetComponentInChildren<CastDown> ();
 		Nav = gameObject.GetComponent<NavMeshAgent> ();
@@ -41,6 +47,7 @@ public class UnitContoller : MonoBehaviour {
 		checkSelfHeat ();
 		Move ();
 		See ();
+		threatAssess ();
 	}
 	void changeSign(){
 		Sign *= -1;
@@ -51,9 +58,9 @@ public class UnitContoller : MonoBehaviour {
 //			Scanning=false;
 			Nav.SetDestination (tileCheck.potentialTile.position);
 		} 
-//		else{
+		else{
 //			Debug.Log("resuming");
-//			Scanning=true;}
+			Nav.SetDestination(transform.position+Vector3.forward);}
 	}
 
 	void checkSelfHeat(){
@@ -70,17 +77,29 @@ public class UnitContoller : MonoBehaviour {
 		float gradient= Sign*Time.deltaTime;
 		transform.Rotate (Vector3.up * gradient*28);
 	}
-
+	void threatAssess(){
+		if (transform.tag == "Red") {
+			Enemies = Manager.BlueCount;
+			Allies=Manager.RedCount-1;
+		} else {
+			Enemies = Manager.RedCount;
+			Allies=Manager.BlueCount-1;
+		}
+	}
 	void See(){
 		RaycastHit hit;
 //		LayerMask mask = ~(1 << LayerMask.NameToLayer ());
-		Ray ray = new Ray (transform.position, Vector3.forward);
-		if (Physics.Raycast (ray, out hit, 3)) {
-			if (hit.collider.tag=="Wall"){
-				Debug.Log("walling"+transform.eulerAngles);
-				transform.Rotate (180*Vector3.up);
+		Ray ray = new Ray (transform.position+0.5f*Vector3.forward, Vector3.forward);
+		if (Physics.Raycast (ray, out hit, 1)) {
+			if (hit.transform.tag=="Wall"){
+				Debug.Log("walling"+transform.eulerAngles+" "+gameObject.name);
+				transform.Rotate (180*Vector3.up-transform.eulerAngles);
 				Debug.Log(transform.eulerAngles);
 			}
+		}
+		if (Physics.Raycast (ray, out hit, 5)) {
+			if (hit.transform.tag=="Red"||hit.transform.tag=="Blue"&&hit.transform.tag!=transform.tag){
+				Debug.Log ("target spotted");}
 		}
 	}
 }
---
+++ Workspace/Assets/SquadManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquadManager : MonoBehaviour {
+	public Transform[] Reds;
+	public Transform[] Blues;
+	public int BlueCount;
+	public int RedCount;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SquadTrack ();
+	}
+
+	void SquadTrack(){
+		BlueCount = 0;
+		RedCount = 0;
+
+		for (int i=0; i<gameObject.transform.GetChildCount(); i++) {
+			if (gameObject.transform.GetChild(i).tag=="Red"){
+				RedCount++;}
+			else{
+				BlueCount++;}
+		}
+		Reds= new Transform[RedCount];
+		Blues = new Transform[BlueCount];
+		int x = 0;
+		int y = 0;
+		for (int i=0; i<gameObject.transform.GetChildCount(); i++) {
+			if (gameObject.transform.GetChild(i).tag=="Red"){
+				Reds[x]=gameObject.transform.GetChild (i);
+				x++;}
+			else{
+				Blues[y]=gameObject.transform.GetChild (i);
+				y++;}
+		}
+	}
+
+	void SetEnemies(){
+		int j;
+		if (transform.GetChild (0).tag == "Red") {
+			j = 1;
+		} else {
+			j = 0;
+		}
+	}
+}
